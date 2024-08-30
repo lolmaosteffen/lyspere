f7d42e2b8a656c966d3e8c68618220796a0ebe61
lolmaosteffen <steffen-1997@live.no>
2024-08-30 12:41:56 +0200

    prøve å legga t if funksjoner

--- kode.js
+++ kode.js
@@ -1,12 +1,29 @@
 function skruPå() {
     document.getElementById("lyspære").style.backgroundColor = 'yellow';
-    document.getElementById("tekst").innerHTML = "Nå er lyspæra på!";
-    document.getElementById("buttons").innerHTML = /*HTML*/`
-        <button id="lysbryter" onclick="skruAv()">Av</button>`;
 }
 function skruAv() {
     document.getElementById("lyspære").style.backgroundColor = 'grey';
-    document.getElementById("tekst").innerHTML = "Nå er lyspæra av!";
+}
+let lysPærePå;
+let lysPæreAv;
+
+if (document.getElementById("lyspære").style.backgroundColor = 'yellow') {
+    lysPærePå = true
+    lysPæreAv = false
+}
+if (document.getElementById("lyspære").style.backgroundColor = 'grey') {
+    lysPærePå = false
+    lysPæreAv = true
+}
+
+
+if (lysPærePå = true) {
     document.getElementById("buttons").innerHTML = /*HTML*/`
-        <button id="lysbryter" onclick="skruPå()">På</button>`;
+    <button id="lysbryter" onclick="skruAv()">Skru av</button>`;
+    document.getElementById("tekst").innerHTML = "Nå er lyspæra på!"
+}
+if (lysPæreAv = true) {
+    document.getElementById("buttons").innerHTML = /*HTML*/`
+        <button id="lysbryter" onclick="skruPå()">Skru på</button>`;
+    document.getElementById("tekst").innerHTML = "Nå er lyspæra av!"
 }
